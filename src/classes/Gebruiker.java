7309bf85997cfb0c0fbef6b869e7f4d671ace1e3
jesse tjiook <jesse.tjiook@me.com>
2023-03-23 10:34:59 +0100

    Constructors, getters en setters geadd in Admin, Gebruiker, Klant en Offerte klassen.

--- src/classes/Gebruiker.java
+++ src/classes/Gebruiker.java
@@ -5,4 +5,43 @@ public class Gebruiker {
     private int ID;
     private String password;
     private String email;
+
+    public Gebruiker(String naam, int ID, String password, String email) {
+        this.naam = naam;
+        this.ID = ID;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/classes/Offerte.java
+++ src/classes/Offerte.java
@@ -13,4 +13,62 @@ public class Offerte {
     private double milieuKorting;
     private double totaalPrijs; // word ge-set vanuit de optionele kosten (van essentieleOpties) en kortingen
 
+
+    public Offerte(int offerteID, String naam, String beschrijving, ArrayList<EssentieleOpties> essentieleOpties, ArrayList<Klant> klant, double milieuKorting, double totaalPrijs) {
+        this.offerteID = offerteID;
+        this.naam = naam;
+        this.beschrijving = beschrijving;
+        this.essentieleOpties = essentieleOpties;
+        this.klant = klant;
+        this.milieuKorting = milieuKorting;
+        this.totaalPrijs = totaalPrijs;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getBeschrijving() {
+        return beschrijving;
+    }
+
+    public void setBeschrijving(String beschrijving) {
+        this.beschrijving = beschrijving;
+    }
+
+    public ArrayList<EssentieleOpties> getEssentieleOpties() {
+        return essentieleOpties;
+    }
+
+    public void setEssentieleOpties(ArrayList<EssentieleOpties> essentieleOpties) {
+        this.essentieleOpties = essentieleOpties;
+    }
+
+    public ArrayList<Klant> getKlant() {
+        return klant;
+    }
+
+    public void setKlant(ArrayList<Klant> klant) {
+        this.klant = klant;
+    }
+
+    public double getMilieuKorting() {
+        return milieuKorting;
+    }
+
+    public void setMilieuKorting(double milieuKorting) {
+        this.milieuKorting = milieuKorting;
+    }
+
+    public double getTotaalPrijs() {
+        return totaalPrijs;
+    }
+
+    public void setTotaalPrijs(double totaalPrijs) {
+        this.totaalPrijs = totaalPrijs;
+    }
 }

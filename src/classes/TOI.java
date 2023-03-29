8df8399840b1d9d47c73f72832937075d71bc3e0
jesse <jesse.tjiook@me.com>
2023-03-29 10:14:29 +0200

    Test Class TOI (TestOfferteInformatie) gemaakt voor een functie die de nieuwe prijs met korting berekent.

---
+++ src/classes/TOI.java
@@ -0,0 +1,25 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class TOI extends Offerte{
+    public TOI(int offerteID, String naam, String beschrijving, ArrayList<EssentieleFeatures> essentieleFeatures, ArrayList<Klant> klant, double milieuKorting, double totaalPrijs) {
+        super(offerteID, naam, beschrijving, essentieleFeatures, klant, milieuKorting, totaalPrijs);
+    }
+
+    public static double nieuwePrijs(double prijs, double korting) {
+        double factor = (korting / 100);
+
+        return prijs * factor;
+    }
+
+    public static void main(String[] args) {
+        double testPrijs1 = 120.50;
+        double testPrijs2 = 567.30;
+        double testPrijs3 = 300.00;
+        double korting = 30.0;
+
+        System.out.println(nieuwePrijs(testPrijs1, korting));
+    }
+
+}

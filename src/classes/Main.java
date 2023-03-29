76411ab2471d08285e6cbc9ef14609f58e4e2d31
SmoothPapi69 <127389972+SmoothPapi69@users.noreply.github.com>
2023-03-29 11:07:11 +0200

    Ik heb het project in maven gezet. En ik heb de json database code toegevoegd.

--- src/classes/Main.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class Main {
-    public static void main(String[] args) {
-        HoofdMenu menu = new HoofdMenu();
-        menu.createMenu();
-        menu.execute(menu.printMenu());
-    }
-}

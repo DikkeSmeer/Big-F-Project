3885413730d54e859e7502a33863c2fdeb202cb3
theArjanMeister <arjanhoeblal@live.nl>
2023-03-29 20:17:29 +0200

    Gangster 29-3-23

--- src/main/java/classes/Gebruiker.java
+++ src/main/java/classes/Gebruiker.java
@@ -1,4 +1,8 @@
 package classes;
+import sun.java2d.loops.GeneralRenderer;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gebruiker {
     private String naam;
@@ -6,11 +10,18 @@ public class Gebruiker {
     private String password;
     private String email;
 
+    private ArrayList<Gebruiker> gebruikers;
+
+
     public Gebruiker(String naam, int ID, String password, String email) {
         this.naam = naam;
         this.ID = ID;
         this.password = password;
         this.email = email;
+    }
+
+    public Gebruiker() {
+        this.gebruikers = new ArrayList<>();
     }
 
     public String getNaam() {
@@ -43,5 +54,57 @@ public class Gebruiker {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public ArrayList<Gebruiker> getGebruikers() {
+        return gebruikers;
+    }
+
+    public void setGebruikers(ArrayList<Gebruiker> gebruikers) {
+        this.gebruikers = gebruikers;
+    }
+
+//    class GebruikerMenu extends Menu {
+//        public void createMenu(){
+//            menu.add(new gebruikerAanmakenItem("Klant Aanmaken"));
+//            menu.add(new gebruikerBekijkenItem("Klanten Bekijken"));
+//            menu.add(new TerugItem("Terug"));
+//        }
+//    }
+    public void maakGebruikerAan(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Voer de naam van de gebruiker in:");
+        String naam = scanner.nextLine();
+
+        System.out.println("Voer het ID van de gebruiker in:");
+        int ID = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Voer het e-mailadres van de gebruiker in:");
+        String email = scanner.nextLine();
+
+        System.out.println("Voer het wachtwoord van de gebruiker in:");
+        String password = scanner.nextLine();
+
+        Gebruiker nieuweGebruiker = new Gebruiker(naam, ID, password, email);
+        gebruikers.add(nieuweGebruiker);
+        scanner.close();
+        System.out.println("Klant is toegevoegd!");
+    }
+    public void verwijderGebruiker(int ID){
+        int j = 0;
+        for (j = 0; j < gebruikers.size(); j++) ;
+        Gebruiker huidigGebruiker = gebruikers.get(j);
+        if (huidigGebruiker.getID() == getID()) {
+            gebruikers.remove(getID());
+            System.out.println("Gebruiker met ID:"+getID()+" is verwijderd");
+        }
+
+    }
+
+    public void bekijkenGebruiker(){
+        for (Gebruiker gebruikers : gebruikers){
+            System.out.println("Naam: " + gebruikers.getNaam() + "/n" + "ID: " + gebruikers.getID() + "/n" + gebruikers.getEmail() + "/n" + "email: " + "/n" + gebruikers.getNaam());
+        }
     }
 }
--- src/main/java/classes/Klant.java
+++ src/main/java/classes/Klant.java
@@ -1,15 +1,30 @@
 package classes;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Klant extends Gebruiker{
     private String klantType;
     private double klantKorting;
 
+    private ArrayList<Klant> klant; // de klant die bij het offerte hoort
     public Klant(String naam, int ID, String password, String email, String klantType, double klantKorting) {
         super(naam, ID, password, email);
         this.klantType = klantType;
         this.klantKorting = klantKorting;
     }
 
+    public ArrayList<Klant> getKlant() {
+        return klant;
+    }
+
+    public void setKlant(ArrayList<Klant> klant) {
+        this.klant = klant;
+    }
+
+    public Klant(){
+        this.klant = new ArrayList<>();
+    }
     public String getKlantType() {
         return klantType;
     }
@@ -18,6 +33,7 @@ public class Klant extends Gebruiker{
         this.klantType = klantType;
     }
 
+
     public double getKlantKorting() {
         return klantKorting;
     }
@@ -25,4 +41,47 @@ public class Klant extends Gebruiker{
     public void setKlantKorting(double klantKorting) {
         this.klantKorting = klantKorting;
     }
+
+    public void maakKlantAan(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Voer uw naam in:");
+        String naam = scanner.nextLine();
+
+        System.out.println("Voer het ID van de klant in:");
+        int ID = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Voer het wachtwoord van de klant in:");
+        String password = scanner.nextLine();
+
+        System.out.println("Voer het emailadres van de klant in:");
+        String email = scanner.nextLine();
+
+        System.out.println("Voer klanttype van de klant in:");
+        String klantType = scanner.nextLine();
+
+        System.out.println("Voer de klantkorting van de klant in:");
+        double klantKorting = scanner.nextDouble();
+
+        Klant nieuweKlant = new Klant(naam, ID, password, email, klantType, klantKorting);
+        klant.add(nieuweKlant);
+        scanner.close();
+        System.out.println("Klant is toegevoegd!");
+    }
+    public void verwijderKlant(int ID) {
+        int i;
+        for (i = 0; i < klant.size(); i++) ;
+        Klant huidigKlant = klant.get(i);
+        if (huidigKlant.getID() == getID()) {
+            klant.remove(getID());
+            System.out.println("Klant met ID:" + getID() + " is verwijderd");
+        }
+    }
+
+    public void bekijkenKlant(){
+        for (Klant klant : klant){
+            System.out.println("Naam: " + klant.getNaam() + "/n" + "ID: " + klant.getID() + "/n" + klant.getEmail() + "/n" + "email: " + "/n" + klant.getNaam());
+        }
+    }
+
 }

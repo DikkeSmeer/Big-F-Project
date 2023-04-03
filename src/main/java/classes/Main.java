c1fd1326101f06d36460332e6a177a0fa6ff51ff
SmoothPapi69 <127389972+SmoothPapi69@users.noreply.github.com>
2023-04-03 15:33:49 +0200

    code toegevoegd wat zorgt dat gebruiker de optie krijgt om klantgegevens te wijzigen en die opnieuw op te slaan in de .json file.

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -5,87 +5,83 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
 
-        /* Save Objects vvv*/
-
-        ObjectSaver<KlantInfo> objectSaver = new ObjectSaver<>("KlantInformatie.json"); // Maak een nieuw ObjectSaver-object voor KlantInfo-objecten met de naam "KlantInformatie.json".
-
-        KlantInfo klant1 = new KlantInfo("Jan", 1, "wachtwoord1", "jan@mail.com", "particulier", 0.05); // Maak een nieuw KlantInfo-object genaamd klant1.
-        KlantInfo klant2 = new KlantInfo("Piet", 2, "wachtwoord2", "piet@mail.com", "particulier", 0.10); // Maak een nieuw KlantInfo-object genaamd klant2.
-        KlantInfo klant3 = new KlantInfo("Klaas", 3, "wachtwoord3", "klaas@mail.com", "bedrijf", 0.15); // Maak een nieuw KlantInfo-object genaamd klant3.
-        KlantInfo klant4 = new KlantInfo("Henk", 4, "wachtwoord4", "henk@mail.com", "bedrijf", 0.20); // Maak een nieuw KlantInfo-object genaamd klant4.
-        KlantInfo klant5 = new KlantInfo("Sjaak", 5, "wachtwoord5", "sjaak@mail.com", "particulier", 0.05); // Maak een nieuw KlantInfo-object genaamd klant5.
-
-        objectSaver.addObject(klant1); // Voeg het klant1-object toe aan de lijst van objecten die opgeslagen moeten worden.
-        objectSaver.addObject(klant2); // Voeg het klant2-object toe aan de lijst van objecten die opgeslagen moeten worden.
-        objectSaver.addObject(klant3); // Voeg het klant3-object toe aan de lijst van objecten die opgeslagen moeten worden.
-        objectSaver.addObject(klant4); // Voeg het klant4-object toe aan de lijst van objecten die opgeslagen moeten worden.
-        objectSaver.addObject(klant5); // Voeg het klant5-object toe aan de lijst van objecten die opgeslagen moeten worden.
-        objectSaver.saveObjects(); // Sla alle objecten in de lijst op in het JSON-bestand met de naam opgeslagen in de instantievariabele fileName van het ObjectSaver-object.
+//        /* Save Objects vvv*/
+        ObjectSaver<KlantInfo> objectSaver = new ObjectSaver<>("KlantInformatie.json"); // Maak een nieuw ObjectSaver-object voor KlantInfo-objecten met de .json file naam "KlantInformatie.json".
+//        /* Save Objects ^^^*/
 
 
-        /* Save Objects ^^^*/
+        /* Load Objects  vvv */
 
+        ObjectLoader<KlantInfo> objectLoader = new ObjectLoader<>(KlantInfo.class, "KlantInformatie.json");
 
-//        /* Load Objects  vvv */
-//
-//        ObjectLoader<KlantInfo> objectLoader = new ObjectLoader<>(KlantInfo.class, "KlantInformatie.json");
-//
 //        List<KlantInfo> loadedObjects = objectLoader.loadObjects();
-//
+
 //        for (KlantInfo obj : loadedObjects) {
 //            System.out.println("\nNaam: " + obj.getNaam() + "\nLeeftijd: " + obj.getLeeftijd() + "\nWachtwoord: " + obj.getPassword() + "\nEmail: " + obj.getEmail() + "\nFunctie: " + obj.getFunctie() + "\nKorting: " + obj.getKorting());
 //        }
-//        /* Load Objects  ^^^ */
+        /* Load Objects  ^^^ */
 
 
 // gebruiker vragen om gegevens in te vullen vvvvv
-//        boolean juisteGegevens = false;
-//
-//        while (!juisteGegevens) {
-//            System.out.print("\nZijn deze klantgegevens correct? (Ja/Nee) ");
-//            String antwoord = scanner.nextLine();
-//
-//            if (antwoord.equalsIgnoreCase("Ja")) {
-//                juisteGegevens = true;
-//            } else if (antwoord.equalsIgnoreCase("Nee")) {
-//                // Vraag om nieuwe gebruikersinformatie
-//                System.out.println("\nVul uw gegevens in:");
-//                System.out.print("Klant naam: ");
-//                String klantNaam = scanner.nextLine();
-//                System.out.print("Klant leeftijd: ");
-//                int klantLeeftijd = scanner.nextInt();
-//                System.out.print("Klant wachtwoord: ");
-//                String klantWachtwoord = scanner.nextLine();
-//                System.out.println("Klant email: ");
-//                String klantEmail = scanner.nextLine();
-//                System.out.println("Klant functie: ");
-//                String klantFunctie = scanner.nextLine();
-//                System.out.println("Klant korting: ");
-//                double klantKorting = scanner.nextDouble();
-//
-//                // wijzig de waarden van de instantie van de klasse Gebruiker
-//                KlantInfo.setNaam(klantNaam);
-//                KlantInfo.setLeeftijd(klantLeeftijd);
-//                KlantInfo.setAdres(klantWachtwoord);
-//                KlantInfo.setPostcode(klantEmail);
-//                KlantInfo.setPlaats(klantFunctie);
-//                KlantInfo.setPlaats(klantKorting);
-//
-//
-//                // Toon de nieuwe gegevens aan de gebruiker ter verificatie
-//                System.out.println("\nHier zijn uw nieuwe gegevens:");
-//                System.out.println("Klant naam: " + gebruiker.getNaamBedrijf());
-//                System.out.println("Klant leeftijd: " + gebruiker.getNaamGebruiker());
-//                System.out.println("Klant wachtwoord: " + gebruiker.getAdres());
-//                System.out.println("Klant email: " + gebruiker.getPostcode());
-//                System.out.println("Klant functie: " + gebruiker.getPlaats());
-//                System.out.println("Klant korting: " + KlantInfo.getBedrijfEmail());
-//            } else {
-//                System.out.println("Niet herkend wat je hebt ingevuld, probeer opnieuw.");
-//            }
-//        }
+
+        boolean juisteGegevens = false;
+
+        while (!juisteGegevens) {
+            System.out.print("\nZijn deze klantgegevens correct? (Ja/Nee) ");
+            String antwoord = scanner.nextLine();
+
+            if (antwoord.equalsIgnoreCase("Ja")) {
+                juisteGegevens = true;
+            } else if (antwoord.equalsIgnoreCase("Nee")) {
+                // Vraag om nieuwe gebruikersinformatie
+                System.out.println("\nVul uw gegevens in:");
+                System.out.print("Klant naam: ");
+                String klantNaam = scanner.nextLine();
+                System.out.print("Klant leeftijd: ");
+                int klantLeeftijd = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("Klant wachtwoord: ");
+                String klantWachtwoord = scanner.nextLine();
+                System.out.println("Klant email: ");
+                String klantEmail = scanner.nextLine();
+                System.out.println("Klant functie: ");
+                String klantFunctie = scanner.nextLine();
+                System.out.println("Klant korting: ");
+                double klantKorting = scanner.nextDouble();
+                scanner.nextLine();
+
+                // wijzig de waarden van de instantie van de klasse Gebruiker
+                KlantInfo Klant = new KlantInfo("", 20, "wachtwoord", "email", "functie", 0.05);
+                Klant.setNaam(klantNaam);
+                Klant.setLeeftijd(klantLeeftijd);
+                Klant.setPassword(klantWachtwoord);
+                Klant.setEmail(klantEmail);
+                Klant.setFunctie(klantFunctie);
+                Klant.setKorting(klantKorting);
+
+                objectSaver.addObject(Klant); // Voeg het klant1-object toe aan de lijst van objecten die opgeslagen moeten worden.
+                objectSaver.saveObjects(); // Sla alle objecten in de lijst op in het JSON-bestand met de naam opgeslagen in de instantievariabele fileName van het ObjectSaver-object.
+
+                List<KlantInfo> loadedObjects = objectLoader.loadObjects();
+                for (KlantInfo obj : loadedObjects) {
+
+                    // Toon de nieuwe gegevens aan de gebruiker ter verificatie
+                    System.out.println("\nHier zijn uw nieuwe gegevens:");
+                    System.out.println("Klant naam: " + obj.getNaam());
+                    System.out.println("Klant leeftijd: " + obj.getLeeftijd());
+                    System.out.println("Klant wachtwoord: " + obj.getPassword());
+                    System.out.println("Klant email: " + obj.getEmail());
+                    System.out.println("Klant functie: " + obj.getFunctie());
+                    System.out.println("Klant korting: " + obj.getKorting());
+
+                }
+            } else {
+                System.out.println("Niet herkend wat je hebt ingevuld, probeer opnieuw.");
+            }
+        }
 // gebruiker vragen om gegevens in te vullen^^^^
 
 
@@ -100,7 +96,7 @@ public class Main {
     }
 }
 
-class KlantInfo {
+class KlantInfo{
     private String naam;
     private int leeftijd;
     private String password;
@@ -141,7 +137,7 @@ class KlantInfo {
         return korting;
     }
 
-    public void setName(String naam) {
+    public void setNaam(String naam) {
         this.naam = naam;
     }
 
@@ -162,7 +158,7 @@ class KlantInfo {
     }
 
     public void setKorting(double korting) {
-        this.email = email;
+        this.korting = korting;
     }
 
 }
